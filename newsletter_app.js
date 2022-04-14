7a232b90a885d43ffe7a4e6e8503ef7957fd1346
asif <mdasifahamed89@yahoo.com>
2022-04-15 02:04:46 +0600

    Adding mailchimp Api

--- newsletter_app.js
+++ newsletter_app.js
@@ -1,6 +1,7 @@
 const express = require('express'); // the express libray for the web application
 const bodyParser = require('body-parser'); // the body-parser libray to get data from user input in html form
 const requst = require('request');
+const https = require('https')
 const app = express(); // iniatilising the application
 var port = 3000; // initialising the prt  3000 is the local port mean this computer is ther server
 app.use(express.static("public"));// static is an express method used to declareing the that contains
@@ -17,6 +18,35 @@ app.post('/', function(req, res){ // this post method comes from the html form i
   var last_name = req.body.lastn; // storing the user data in avariable using body-parser and html
   var e_mail = req.body.email; // storing the user data in avariable using body-parser and html
   console.log("The name  of the subscriber is " + first_name + " " + last_name + " and his email address is : " + e_mail);
+
+  var data = {
+  "members": [
+    {
+
+      email_address: e_mail,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: first_name,
+        LNAME: last_name
+      }
+    }
+  ]
+}
+const json_data = JSON.stringify(data);
+const url  = "https://us14.api.mailchimp.com/3.0/lists/7c73dd23b1"
+const options = {
+  method : "POST",
+  auth: "asif:442ee83d244b677080faf174ae7ff6f1-us14"
+}
+
+const request = https.request(url,options, function(response){
+  response.on("data",function(data){
+    console.log(JSON.parse(data));
+  })
+
+})
+request.write(json_data);
+request.end();
 })
 
 
@@ -25,3 +55,8 @@ app.post('/', function(req, res){ // this post method comes from the html form i
 app.listen(port,function(){
   console.log("Server Is Running");
 })
+
+
+//Api key Mailchimp
+//442ee83d244b677080faf174ae7ff6f1-us14
+//list_id for the api " 7c73dd23b1 "
